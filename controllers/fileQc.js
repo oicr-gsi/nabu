783fb1acbcaeb6521e3420425e53799282f22e51
Heather Armstrong <heather.e.armstrong@gmail.com>
2017-11-08 15:15:37 -0500

    use custom validation error

--- controllers/fileQc.js
+++ controllers/fileQc.js
@@ -10,9 +10,20 @@ module.exports = {
   addManyFileQcs: addManyFileQcs
 };
 
+// set up custom error if bad params are given
+function ValidationError(message) {
+  this.message = message;
+}
+ValidationError.prototype = Error.prototype;
+
 function getFileQcBySwid(req, res, next) {
-  const swid = validateSwid(req.params.identifier, next);
-  
+  let swid;
+  try {
+    swid = validateSwid(req.params.identifier, next);
+  } catch (e) {
+    if (e instanceof ValidationError) return next(generateError(400, e.message));
+  }
+
   const sql = 'SELECT * FROM FileQC WHERE fileswid = $1';
   db.any(sql, [swid])
     .then(data => { 
@@ -34,7 +45,12 @@ function getFileQcBySwid(req, res, next) {
 
 function getAllFileQcs(req, res, next) {
   // access the project param
-  const proj = validateProject(req.query.project, next);
+  let proj;
+  try {
+    proj = validateProject(req.query.project);
+  } catch (e) {
+    if (e instanceof ValidationError) return next(generateError(400, e.message));
+  }
 
   const sql = 'SELECT * FROM FileQC WHERE project = $1';
   db.any(sql, [proj])
@@ -53,13 +69,18 @@ function getAllFileQcs(req, res, next) {
 
 function addFileQc(req, res, next) {
   // TODO: fix this
-  const project = validateProject(req.query.project, next);
-  const filePath = validateFilepath(req.query.filepath, next);
-  const fileSWID = validateSwid(req.query.fileswid, next);
-  const username = validateUsername(req.query.username, next);
-  const comment = validateComment(req.query.comment, res);
-  const qcPassed = convertQcStatusToBoolean(req.query.qcstatus);
-  if (qcPassed === null) return next(generateError(400, 'FileQC must be saved with status "PASS" or "FAIL"'));
+  let project, filePath, fileSWID, username, comment, qcPassed;
+  try {
+    project = validateProject(req.query.project);
+    filePath = validateFilepath(req.query.filepath);
+    fileSWID = validateSwid(req.query.fileswid);
+    username = validateUsername(req.query.username);
+    comment = validateComment(req.query.comment);
+    qcPassed = convertQcStatusToBoolean(req.query.qcstatus);
+    if (qcPassed === null) throw new ValidationError('FileQC must be saved with status "PASS" or "FAIL"');
+  } catch (e) {
+    if (e instanceof ValidationError) return next(generateError(400, e.message));
+  }
 
   // update if exists, insert if not
   const upsert = 'INSERT INTO FileQc as fqc (filepath, qcpassed, username, comment, fileswid, project) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (fileswid) DO UPDATE SET filepath = $1, qcpassed = $2, username = $3, comment = $4 WHERE fqc.fileswid = $5';
@@ -82,25 +103,39 @@ function addFileQc(req, res, next) {
 }
 
 function addManyFileQcs(req, res, next) {
+  if (!req.body) return next(generateError(400, 'Error: no FileQCs found in request body"\'));
+  const validationResults = validateObjectsFromUser(req.body.fileqcs, req.body.project);
+  if (valiationResults.errors.length) return next(generateError(400, validationResults.errors));
 
+  const upsert = 'INSERT INTO FileQc as fqc (filepath, qcpassed, username, comment, fileswid, project) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (fileswid) DO UPDATE SET filepath = $1, qcpassed = $2, username = $3, comment = $4 WHERE fqc.fileswid = $5';
+
+  db.tx('batch', t => {
+
+  })
+  .then(data => {
+
+  })
+  .catch(err => {
+
+  })
 }
 
 // validation functions
-function validateProject(param, next) {
+function validateProject(param) {
   const proj = param || null;
-  if (proj == null || !proj.length) return next(generateError(400, 'Error: project must be provided'));
+  if (proj == null || !proj.length) throw new ValidationError('Project must be provided');
   return proj;
 }
 
-function validateSwid(param, next) {
+function validateSwid(param) {
   const swid = parseInt(param);
-  if (Number.isNaN(swid)) return next(generateError(400, 'Error: swid is ' + param + ' but must be an integer'));
+  if (Number.isNaN(swid)) throw new ValidationError('FileSWID is ' + param + ' but must be an integer');
   return swid;
 }
 
-function validateUsername(param, next) {
+function validateUsername(param) {
   const user = nullifyIfBlank(param);
-  if (user == null || !user.length) return next(generateError(400, 'Error: username must be provided'));
+  if (user == null || !user.length) throw new ValidationError('Username must be provided');
   return user;
 }
 
@@ -110,9 +145,9 @@ function validateComment(param) {
   return comment;
 }
 
-function validateFilepath(param, next) {
+function validateFilepath(param) {
   let fp = nullifyIfBlank(param);
-  if (fp == null || !fp.length) return next(generateError(400, 'Error: filepath must be provided'));
+  if (fp == null || !fp.length) throw new ValidationError('Filepath must be provided');
   fp = decodeURIComponent(fp.replace(/\+/g, ' '));
   return fp;
 }
@@ -137,4 +172,29 @@ function convertQcStatusToBoolean(value) {
     'pending': null
   };
   return statusToBool[value.toLowerCase()];
+}
+
+function validateObjectsFromUser(unvalidatedObjects, unvalidatedProject) {
+  // returns an object { validated: [], errors: [] }
+  let validationErrors = [];
+  let validatedObjects = unvalidatedObjects.map(validateFileQcObject);
+  return { validated: validatedObjects, errors: validationErrors };
+  
+  function validateFileQcObject(unvalidated) {
+    const validated = {};
+    try {
+      validated.project = validateProject(unvalidatedProject);
+      validated.filepath = validateFilepath(unvalidated.filepath);
+      validated.fileswid = validateSwid(unvalidated.fileswid);
+      validated.username = validateUsername(unvalidated.username);
+      validated.comment = validateComment(unvalidated.comment);
+      validated.qcpassed = validateQcPassed(unvalidated.qcstatus);
+      if (qcPassed === null) throw new ValidationError('FileQC must be saved with status "PASS" or "FAIL"');
+    } catch (e) {
+      if (e instanceof ValidationError) {
+        validationErrors.push({ fileswid: unvalidated.fileswid, error: e.message });
+        return null;
+      }
+    }
+  }
 }
