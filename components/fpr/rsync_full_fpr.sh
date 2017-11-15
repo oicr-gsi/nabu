017f438363adce7211704bdd1056227db530b941
Heather Armstrong <heather.armstrong@oicr.on.ca>
2017-11-15 11:20:40 -0500

    Use timestamped copies of the shorter FPR instead of replacing one

--- components/fpr/rsync_full_fpr.sh
+++ components/fpr/rsync_full_fpr.sh
@@ -5,12 +5,14 @@ set -eux
 # rsync the latest file provenance report to local machine
 rsync -vPL "${FPR_FULL}" "${LOCAL_FPR_FULL_DEST}"
 
+now=$(date +"%y%m%d-%H%M%S")
+
 # extract only the desired fields, and only unique File SWIDs
 # 45. File SWID
 # 47. File Path
 # 52. Skip [true|false]
 # 53. Status [OKAY|STALE|ERROR]
 # 2. Study Title
-zcat "${LOCAL_FPR_FULL_DEST}"/*.tsv.gz | awk -F'\t' '!seen[$45] { print $45"\t"$47"\t"$52"\t"$53"\t"$2; seen[$45] = 1; }' > "${LOCAL_FPR_FULL_DEST}"/fpr.tsv
+zcat "${LOCAL_FPR_FULL_DEST}"/*.tsv.gz | awk -F'\t' '!seen[$45] { print $45"\t"$47"\t"$52"\t"$53"\t"$2; seen[$45] = 1; }' | sort -g -t$'\t' -k1 > "${LOCAL_FPR_FULL_DEST}"/"${now}"-fpr.tsv
 
 exit 0
