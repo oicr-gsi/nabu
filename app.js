775ac95974b389c088c147a92d1f0d6f81c5ba40
Heather Armstrong <heather.armstrong@oicr.on.ca>
2018-02-16 17:32:02 -0500

    Log unexpected errors instead of throwing them into the void

--- app.js
+++ app.js
@@ -19,8 +19,15 @@ const errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
-  res.status(err.status || 500);
-  res.json({ 'errors': err.errors });
+  if (!err.status) {
+    // unexpected error, so log it
+    logger.error({uid: req.uid, message: err.message });
+    res.status(500);
+    res.json({ 'errors': ['An unexpected error has occurred.']});
+  } else {
+    res.status(err.status);
+    res.json({ 'errors': err.errors });
+  }
   res.end();
   next();
 };
