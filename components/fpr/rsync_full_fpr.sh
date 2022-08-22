506b068112e6e7434bee0307fdb8252ee16ac5f9
Heather Armstrong <heather.armstrong@oicr.on.ca>
2022-08-22 16:14:45 -0400

    Add new columns to databases

--- components/fpr/rsync_full_fpr.sh
+++ components/fpr/rsync_full_fpr.sh
@@ -13,15 +13,18 @@ now=$(date +"%y%m%d-%H%M%S")
 # Extract only the desired fields, and only unique File SWIDs
 # Skip the first line as we'll be importing the .tsv into a database
 # 45. File SWID
+# 46. File Attributes (contains File SWID)
 # 47. File Path
+# 48. File Md5sum
 # 52. Skip [true|false]
 # 53. Status [OKAY|STALE|ERROR]
 # 2. Study Title
 # 39. Workflow Run Input File SWAs
+# 31. Workflow Name
 # 14. Sample Name
 # 19. Sequencer Run Name
 # 25. Lane Number
-zcat "${SQLITE_LOCATION}"/*.tsv.gz | awk -F'\t' '!seen[$45] && NR>1 { print $45"\t"$47"\t"$52"\t"$53"\t"$2"\t"$39"\t"$31"\t"$14"\t"$19"\t"$25; seen[$45] = 1; }' | sort -g -t$'\t' -k1 > "${FPR_SMALL_DEST}"/"${now}"-fpr.tsv
+zcat "${SQLITE_LOCATION}"/*.tsv.gz | awk -F'\t' '!seen[$45] && NR>1 { print $45"\t"$46"\t"$47"\t"$48"\t"$52"\t"$53"\t"$2"\t"$39"\t"$31"\t"$14"\t"$19"\t"$25; seen[$45] = 1; }' | awk -F'\t' '{$2=gensub(/.*niassa-file-accession=([0-9]+).*/,"\\1","1",$2); print}' | sort -g -t$'\t' -k1 > "${FPR_SMALL_DEST}"/"${now}"-fpr.tsv
 
 # symlink the latest one into the folder which contains the database file
 ln -sf "${FPR_SMALL_DEST}"/"${now}"-fpr.tsv "${SQLITE_LOCATION}"/fpr-latest.tsv
