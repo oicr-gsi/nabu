d82fd8fbe4691cd2fb122c07d696944f5e585095
Heather Armstrong <heather.armstrong@oicr.on.ca>
2022-10-17 15:08:24 -0400

    keep most recent fileqc for a given file swid, and delete the rest

--- sql/V007__niassa_ids_to_vidarr_ids.sql
+++ sql/V007__niassa_ids_to_vidarr_ids.sql
@@ -1,3 +1,13 @@
+-- fileqc records now will update instead of adding new fileqcs each time.
+-- keep only the most recent fileqc record for each fileid
+DELETE FROM fileqc WHERE qcdate <> 
+  (SELECT MAX(qcdate) FROM fileqc f2
+    WHERE f2.fileswid = fileqc.fileswid);
+-- and then another select in case of exact timestamp matches
+DELETE FROM fileqc WHERE fileqcid <> 
+  (SELECT MAX(fileqcid) FROM fileqc f2
+    WHERE f2.fileswid = fileqc.fileswid);
+
 -- a fileswid listed in a fileqc row might not have been migrated to Vidarr, so use a default
 CREATE SEQUENCE fileid_seq;
 ALTER TABLE fileqc ADD COLUMN fileid VARCHAR(100) NOT NULL DEFAULT nextval('fileid_seq'::regclass)::VARCHAR;
