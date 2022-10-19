957c09f82efacda568cc198366f0dbc0f03ac3da
Heather Armstrong <heather.armstrong@oicr.on.ca>
2022-10-19 09:48:25 -0400

    Track deletion in a separate table

--- sql/V007__niassa_ids_to_vidarr_ids.sql
+++ sql/V007__niassa_ids_to_vidarr_ids.sql
@@ -16,5 +16,23 @@ CREATE UNIQUE INDEX IF NOT EXISTS fileid_unique_index ON FileQc (fileid);
 
 ALTER TABLE fileqc ADD COLUMN md5sum VARCHAR(100);
 
+ALTER TABLE fileqc ADD COLUMN workflow VARCHAR(100) NOT NULL DEFAULT 'workflow';
+
 -- going forward, fileqc records won't have fileswids, so make this column nullable
 ALTER TABLE fileqc ALTER COLUMN fileswid DROP NOT NULL;
+
+CREATE TABLE deletionlog (
+  deletionid SERIAL PRIMARY KEY,
+  fileid VARCHAR(100) NOT NULL,
+  username VARCHAR(100) NOT NULL,
+  deleteddate TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- populate the deletion log with fileqcs that have already been deleted, then delete them from the table
+INSERT INTO deletionlog (fileid, username)
+  SELECT fileid, username FROM fileqc
+    WHERE deleted IS TRUE;
+
+DELETE FROM fileqc WHERE deleted IS TRUE;
+
+ALTER TABLE fileqc DROP COLUMN deleted;
