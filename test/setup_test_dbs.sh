cdf6b6b0e2a7fe8a81fa8491bbc6d4d68a09da6d
Heather Armstrong <heather.armstrong@oicr.on.ca>
2023-01-31 15:53:45 -0500

    Keep all test database configuration in a single file
    Reduce the duplication across (so many) files

--- test/setup_test_dbs.sh
+++ test/setup_test_dbs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-PG_DB_CONTAINER_NAME="pgtestdb"  # Also used in package.json to stop the container after the test
+# source the .testenv file to use its variables
+. test/.env
 
-# stop any previously-running test containers
-docker stop "${PG_DB_CONTAINER_NAME}"
+docker stop "${PG_DB_CONTAINER_NAME}" || true  # it's fine if the container isn't running
+
+# create flyway.conf file
+echo "flyway.url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}" > test/flyway.conf
+echo "flyway.user=${DB_USER}" >> test/flyway.conf
+echo "flyway.password=${DB_PW}" >> test/flyway.conf
+echo "flyway.cleanDisabled=false" >> test/flyway.conf
+
 # set up the Postgres database
-docker run -d --rm --name "${PG_DB_CONTAINER_NAME}" -p 5436:5432 -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -e POSTGRES_DB=qcdbtest postgres:12-alpine -c shared_buffers=500MB -c fsync=off 
+docker run -d --rm --name "${PG_DB_CONTAINER_NAME}" -p 5436:5432 -e POSTGRES_USER="${DB_USER}" -e POSTGRES_PASSWORD="${DB_PW}" -e POSTGRES_DB="${DB_NAME}" postgres:12-alpine -c shared_buffers=500MB -c fsync=off 
 
 # since we need to mount all the migrations into a single directory, the "regular" migrations need to be
 # copied into the test folder, then deleted after.
