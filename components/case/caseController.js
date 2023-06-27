47f7f6e7c2e75afb2bb6441789dee681db91e165
Heather Armstrong <heather.armstrong@oicr.on.ca>
2023-06-27 14:36:19 -0400

    GP-3987 create an endpoint to create new case and archive

--- components/case/caseController.js
+++ components/case/caseController.js
@@ -2,14 +2,26 @@
 
 const caseDao = require('./caseDao');
 const handleErrors = require('../../utils/controllerUtils').handleErrors;
+const ValidationError = require('../../utils/controllerUtils').ValidationError;
 const logger = require('../../utils/logger').logger;
+
+function arraysEquals (array1, array2) {
+  return (
+    array1.every((item) => array2.includes(item)) &&
+    array2.every((item) => array1.includes(item))
+  );
+}
 
 const getCase = async (req, res, next) => {
   try {
     const cardeaCase = await caseDao.getByCaseIdentifier(
       req.params.caseIdentifier
     );
-    res.status(200).json(cardeaCase);
+    if (cardeaCase) {
+      res.status(200).json(cardeaCase);
+    } else {
+      res.status(404).end();
+    }
   } catch (e) {
     handleErrors(e, 'Error getting case', logger, next);
   }
@@ -17,7 +29,41 @@ const getCase = async (req, res, next) => {
 
 const addCases = async (req, res, next) => {
   try {
-    await caseDao.addCases(req.body);
+    const existingCase = await caseDao.getByCaseIdentifier(
+      req.body.caseIdentifier
+    );
+    if (existingCase) {
+      if (
+        existingCase.requisitionId == req.body.requisitionId &&
+        arraysEquals(existingCase.limsIds, req.body.limsIds) &&
+        arraysEquals(
+          existingCase.workflowRunIdsForOffsiteArchive,
+          req.body.workflowRunIdsForOffsiteArchive
+        ) &&
+        arraysEquals(
+          existingCase.workflowRunIdsForVidarrArchival,
+          req.body.workflowRunIdsForVidarrArchival
+        )
+      ) {
+        // case has already been created with the same data
+        res.status(200).end();
+      } else {
+        // case has already been created, with different data. Will not update. Yell.
+        handleErrors(
+          new ValidationError(
+            'Data provided differs from data for matching existing case. Provided: ' +
+              JSON.stringify(req.body) +
+              '. Existing: ' +
+              JSON.stringify(existingCase)
+          ),
+          'Error creating case',
+          logger,
+          next
+        );
+      }
+    } else {
+      await caseDao.addCases(req.body);
+    }
     res.status(201).end();
   } catch (e) {
     handleErrors(e, 'Error adding cases', logger, next);
--- test/archiveTest.js
+++ test/archiveTest.js
@@ -25,7 +25,6 @@ const addCaseArchives = (server, requestBody = {}) => {
     .send(requestBody);
 };
 
-
 const getCaseByCaseIdentifier = (server, caseIdentifier = {}) => {
   return chai
     .request(server)
@@ -61,11 +60,76 @@ describe('case archive tracking', () => {
       caseIdentifier: 'R22_TEST_0022_Bb_B',
       requisitionId: 22,
       limsIds: ['2222_1_LDI2222', '2222_1_LDI2323', '2222_1_LDI2442'],
-      workflowRunIdsForOffsiteArchive: ['vidarr:research/run/asdf', 'vidarr:research/run/1234'],
-      workflowRunIdsForVidarrArchival: ['vidarr:research/run/abba']
+      workflowRunIdsForOffsiteArchive: [
+        'vidarr:research/run/asdf',
+        'vidarr:research/run/1234',
+      ],
+      workflowRunIdsForVidarrArchival: ['vidarr:research/run/abba'],
     };
     addCaseArchives(server, reqBody).end((err, res) => {
       expect(res.status).to.equal(201);
+      done();
+    });
+  });
+  it('it should return OK when the same case data is submitted', (done) => {
+    let caseIdentifier = 'R11_TEST_1000_Xy_Z';
+    let reqBody = {
+      caseIdentifier: caseIdentifier,
+      requisitionId: 11,
+      limsIds: ['109_1_LDI5432', '109_1_LDI4321'],
+      workflowRunIdsForOffsiteArchive: [
+        'vidarr:research/run/f77732c812aa134f61b3a7c11d1c4451cefe70e90e828a11345e8a0cd7704a0f',
+        'vidarr:research/run/eeb4c43908e5df3dd4997dcc982c4c0d7285b51d7a800e501da06add9125faa7',
+        'vidarr:research/run/e651c4aa01d506904bc8b89a411e948c24d43fc0e841486937f23d72eb7c4fae',
+        'vidarr:research/run/de7b18bb97916885afbb7b085d61f00cfaa28793a8b7260b50c4d4ece3567216',
+      ],
+      workflowRunIdsForVidarrArchival: [
+        'vidarr:research/run/da0e6032ed08591ae684a015ad3c58867a47a65b6c61995e421fc417e2c438c1',
+      ],
+    };
+    getCaseByCaseIdentifier(server, caseIdentifier).end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body.caseIdentifier).to.be.equal(reqBody.caseIdentifier);
+      expect(res.body.requisitionId).to.be.equal(reqBody.requisitionId);
+      expect(res.body.limsIds).to.include.members(reqBody.limsIds);
+      expect(res.body.workflowRunIdsForOffsiteArchive).to.include.members(
+        reqBody.workflowRunIdsForOffsiteArchive
+      );
+      expect(res.body.workflowRunIdsForVidarrArchival).to.include.members(
+        reqBody.workflowRunIdsForVidarrArchival
+      );
+
+      addCaseArchives(server, reqBody).end((err, res) => {
+        expect(res.status).to.equal(200); // 200 means it's the same
+      });
+      done();
+    });
+  });
+  it('it should error when a case is submitted with the same case identifier but different case data', (done) => {
+    let caseIdentifier = 'R11_TEST_1000_Xy_Z';
+    let reqBody = {
+      caseIdentifier: caseIdentifier,
+      requisitionId: 1111111,
+      limsIds: ['109_1_LDI5432', '109_1_LDI4321'],
+      workflowRunIdsForOffsiteArchive: [
+        'vidarr:research/run/f77732c812aa134f61b3a7c11d1c4451cefe70e90e828a11345e8a0cd7704a0f',
+        'vidarr:research/run/eeb4c43908e5df3dd4997dcc982c4c0d7285b51d7a800e501da06add9125faa7',
+        'vidarr:research/run/e651c4aa01d506904bc8b89a411e948c24d43fc0e841486937f23d72eb7c4fae',
+        'vidarr:research/run/de7b18bb97916885afbb7b085d61f00cfaa28793a8b7260b50c4d4ece3567216',
+      ],
+      workflowRunIdsForVidarrArchival: [
+        'vidarr:research/run/da0e6032ed08591ae684a015ad3c58867a47a65b6c61995e421fc417e2c438c1',
+      ],
+    };
+    getCaseByCaseIdentifier(server, caseIdentifier).end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body.caseIdentifier).to.be.equal(reqBody.caseIdentifier);
+      expect(res.body.requisitionId).not.to.be.equal(reqBody.requisitionId);
+
+      addCaseArchives(server, reqBody).end((err, res) => {
+        console.log(res.body);
+        expect(res.status).to.equal(400);
+      });
       done();
     });
   });
