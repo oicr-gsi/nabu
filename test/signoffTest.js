2932346fb84bb875c1ae48b3b51f95681d5f1991
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2024-04-26 14:21:23 -0400

    fixup

--- test/signoffTest.js
+++ test/signoffTest.js
@@ -64,7 +64,7 @@ const getAllSignoffs = (server) => {
 
 describe('case sign-off tracking', () => {
   before(function () {
-    this.timeout(5000);
+    this.timeout(10000);
     cmd.runSync('npm run fw:test-clean; npm run fw:test-migrate');
   });
 
