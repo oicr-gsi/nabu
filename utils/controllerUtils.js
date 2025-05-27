cacfcd5ec3ed9f9737b2f8e58b7c27e6e192619e
Heather Armstrong <harmstrong@oicr.on.ca>
2025-05-27 15:04:34 -0400

    Log more errors

--- utils/controllerUtils.js
+++ utils/controllerUtils.js
@@ -44,13 +44,16 @@ function handleErrors (e, defaultMessage, logger, next) {
     next(generateError(400, e.message));
   } else if (e instanceof NotFoundError) {
     if (process.env.DEBUG == 'true') console.log(e);
+    logger.info(e);
     next(generateError(404, null));
   } else if (e instanceof ConflictingDataError) {
+    logger.error(e);
     next(generateError(409, e.message));
   } else if (e instanceof AuthenticationError) {
+    logger.info(e);
     next(generateError(401, e.message));
   } else if (e.status) {
-    logger.info({ error: e.errors });
+    logger.info(e);
     return next(e); // generateError has already been called, usually because it's a user error
   } else {
     console.log(e);
